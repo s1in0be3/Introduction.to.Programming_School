01f4432a875c0704361f4ab3e708b3c8a701f02d
Siri <72072779+Siibee@users.noreply.github.com>
2020-12-16 03:12:37 +0100

    Update Module1, Prax2.py

--- Module1, Prax2.py
+++ Module1, Prax2.py
@@ -1,4 +1,5 @@
 #! python3
+#PRAX 1
 a=("X")
 b=("|")
 c=("-")
@@ -10,17 +11,23 @@ print("     ",b,"       ",b)
 print("     ",b,"  abc  ",b)
 print("      ",c,c,c,c,c)
 
+#PRAX 2
 print()
 print()
 a = input("Enter Name: ")
 b = input("Home address: ")
 c = input("Phone number: ")
 d = input("Email Address: ")
+
+#Old code, (first time coding ever):
+#print("Your home address is,", b)
+#print("Your number is,", c)
+#print("Your email is,", d)
+#print("Thank you!")
+#print("Output complete!")
+
+#Updated code:
 print ()
-print("Good Morning,", a)
-print("Your home address is,", b)
-print("Your number is,", c)
-print("Your email is,", d)
-print("Thank you!")
+print("\t","Good Morning,", a,"\n","\t","Your home address is,", b,"\n", "\t", "Your number is,", c, "\n", "\t", "Your email is,", d,"\n", "\t", "Thank you!")
 print ()
-print("Output complete!")
+print("\t","Output complete!")
