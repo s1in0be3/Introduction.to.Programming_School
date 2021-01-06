3f5fa44632838448db95d4b254f72d85496948cd
Siri <72072779+Siibee@users.noreply.github.com>
2021-01-06 23:30:36 +0100

    Update Module1, lecture2.py

--- Module1, lecture2.py
+++ Module1, lecture2.py
@@ -7,3 +7,11 @@ Complex: the definition for complex numbers is outside the scope of this lesson.
 Strings and characters:
 A character is a single letter, digit or punctuation symbol.
 A string is basically a sequence of characters.
+
+Note that the values are printed with spaces between them.
+It is, however, possible to override the separator between the listed items from the default single space by
+using the statement sep= as an extra variable in the print() statement.
+
+Every print() statement ends in a newline character \n at a default.
+It is possible to override this by adding an end= statement as an extra variable to the print() statement.
+This may be used to remove the line break or newline functionality.
