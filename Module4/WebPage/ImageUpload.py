9125fd045c0e349883072abb2b9ba24e366efeae
Siri <72072779+Siibee@users.noreply.github.com>
2021-01-24 22:41:14 +0100

    Create ImageUpload.py

---
+++ Module4/WebPage/ImageUpload.py
@@ -0,0 +1,22 @@
+import cgi, os
+
+data = cgi.FieldStorage()
+upload = data["imagefile"] 
+filename = os.path.basename(upload.filename)
+
+with open(filename, "wb") as copy:
+	copy.write(upload.file.read())
+
+print("Content-Type: text/html")
+print()
+print("<!DOCTYPE HTML>")
+print("<html>")
+print("<head>")
+print("<title>Student details</title>")
+print("</head>")
+print("<body>")
+print("<h1>{}</h1>".format(filename))
+print("<img src=\"{}\"/><br/><br/>".format(filename))
+print("<a href=\"imageupload.html\">Back</a>")
+print("</body>")
+print("</html>")
